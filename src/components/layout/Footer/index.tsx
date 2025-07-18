15246dd5bee798da1c6a41a5776d79245311fd5e
HayateIwamoto <iwamoto.hayate.613@gmail.com>
2025-07-19 00:07:33 +0900

    feat: Footerコンポーネントの追加

--- src/components/layout/Footer/index.tsx
+++ src/components/layout/Footer/index.tsx
@@ -3,41 +3,41 @@ import IconList from "@/components/ui/IconList";
 
 const Footer = () => {
   return (
-    <footer className="footer" id="footer">
-      <div className="footer__inner inner">
-        <nav className="footer__nav">
-          <ul className="footer__nav-list">
-            <li className="footer__nav-item">
-              <Link href="/" className="footer__nav-link">
+    <footer className="l-footer" id="footer">
+      <div className="l-footer__inner inner">
+        <nav className="l-footer__nav">
+          <ul className="l-footer__nav-list">
+            <li className="l-footer__nav-item">
+              <Link href="/" className="l-footer__nav-link">
                 Top
               </Link>
             </li>
-            <li className="footer__nav-item">
-              <Link href="/about" className="footer__nav-link">
+            <li className="l-footer__nav-item">
+              <Link href="/about" className="l-footer__nav-link">
                 About
               </Link>
             </li>
-            <li className="footer__nav-item">
-              <Link href="/works" className="footer__nav-link">
+            <li className="l-footer__nav-item">
+              <Link href="/works" className="l-footer__nav-link">
                 Works
               </Link>
             </li>
-            <li className="footer__nav-item">
-              <Link href="/articles" className="footer__nav-link">
+            <li className="l-footer__nav-item">
+              <Link href="/articles" className="l-footer__nav-link">
                 Articles
               </Link>
             </li>
-            <li className="footer__nav-item">
-              <Link href="/contact" className="footer__nav-link">
+            <li className="l-footer__nav-item">
+              <Link href="/contact" className="l-footer__nav-link">
                 Contact
               </Link>
             </li>
           </ul>
         </nav>
-        <div className="footer__icon-list">
+        <div className="l-footer__icon-list">
           <IconList />
         </div>
-        <div className="footer__copyright">
+        <div className="l-footer__copyright">
           <small>&copy; {new Date().getFullYear()} Kaze</small>
         </div>
       </div>
