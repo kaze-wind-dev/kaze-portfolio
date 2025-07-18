62a531ebcec7de194ad8640b9ee1261032533ef5
HayateIwamoto <iwamoto.hayate.613@gmail.com>
2025-07-19 00:02:53 +0900

    style: headerスタイルの調整

--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -4,30 +4,30 @@ import IconList from "@/components/ui/IconList";
 
 const Header = () => {
   return (
-    <header className="header" id="header">
-      <div className="header__inner">
-        <Logo className="header__logo" />
-        <div className="header__line">
-          <nav className="header__nav">
-            <ul className="header__nav-list">
-              <li className="header__nav-item">
-                <Link href="/" className="header__nav-link">Top</Link>
+    <header className="l-header" id="header">
+      <div className="l-header__inner">
+        <Logo className="l-header__logo" />
+        <div className="l-header__line">
+          <nav className="l-header__nav">
+            <ul className="l-header__nav-list">
+              <li className="l-header__nav-item">
+                <Link href="/" className="l-header__nav-link">Top</Link>
               </li>
-              <li className="header__nav-item">
-                <Link href="/about" className="header__nav-link">About</Link>
+              <li className="l-header__nav-item">
+                <Link href="/about" className="l-header__nav-link">About</Link>
               </li>
-              <li className="header__nav-item">
-                <Link href="/works" className="header__nav-link">Works</Link>
+              <li className="l-header__nav-item">
+                <Link href="/works" className="l-header__nav-link">Works</Link>
               </li>
-              <li className="header__nav-item">
-                <Link href="/articles" className="header__nav-link">Articles</Link>
+              <li className="l-header__nav-item">
+                <Link href="/articles" className="l-header__nav-link">Articles</Link>
               </li>
-              <li className="header__nav-item">
-                <Link href="/contact" className="header__nav-link">Contact</Link>
+              <li className="l-header__nav-item">
+                <Link href="/contact" className="l-header__nav-link">Contact</Link>
               </li>
             </ul>
           </nav>
-          <div className="header__icon-list">
+          <div className="l-header__icon-list">
             <IconList />
           </div>
         </div>
