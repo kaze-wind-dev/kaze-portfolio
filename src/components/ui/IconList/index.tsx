21c7932f5009577281f8b75ea4979218a5511aaf
HayateIwamoto <iwamoto.hayate.613@gmail.com>
2025-07-18 23:59:33 +0900

    chore: 外部リンクの追加

--- src/components/ui/IconList/index.tsx
+++ src/components/ui/IconList/index.tsx
@@ -2,19 +2,19 @@ import Link from "next/link";
 
 import { FaGithub } from "react-icons/fa";
 import { SiZenn } from "react-icons/si";
-import { zennLink, githubLink } from "@/constants";
+import { ZENN_LINK, GITHUB_LINK } from "@/constants";
 import styles from "./index.module.scss";
 
 const IconList = () => {
   return (
     <ul className={`${styles.iconList}`}>
       <li className={`${styles.iconList__item}`}>
-        <Link className={`${styles.iconList__link}`} href={githubLink} target="_blank">
+        <Link className={`${styles.iconList__link}`} href={GITHUB_LINK} target="_blank">
           <FaGithub />
         </Link>
       </li>
       <li className={`${styles.iconList__item}`}>
-        <Link className={`${styles.iconList__link}`} href={zennLink} target="_blank">
+        <Link className={`${styles.iconList__link}`} href={ZENN_LINK} target="_blank">
           <SiZenn color="#3EA8FF"/>
         </Link>
       </li>
---
+++ src/constants/index.tsx
@@ -0,0 +1,3 @@
+
+export const ZENN_LINK = 'https://zenn.dev/kaze_wind';
+export const GITHUB_LINK = 'https://github.com/kaze-wind-dev';
