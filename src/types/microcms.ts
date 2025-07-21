434be844627538a8e48baa890b122da287cc57aa
HayateIwamoto <iwamoto.hayate.613@gmail.com>
2025-07-21 19:31:25 +0900

    chore: microcmsの連携を設定

---
+++ src/types/microcms.ts
@@ -0,0 +1,25 @@
+import type { MicroCMSListContent, MicroCMSImage } from "microcms-js-sdk";
+
+export type Works = {
+  title: string;
+  description: string;
+  site_url: string;
+  design_url: string;
+  period: string;
+  background: string;
+  technical: string;
+  learned: string;
+  time: string;
+  thumbnail?: MicroCMSImage;
+  images?: MicroCMSImage[];
+  category: {
+    name: string;
+  };
+  technology_stack: string[];
+} & MicroCMSListContent;
+
+export type Category = {
+  name: string;
+} & MicroCMSListContent;
+
+export type WorksCategory = Category;
