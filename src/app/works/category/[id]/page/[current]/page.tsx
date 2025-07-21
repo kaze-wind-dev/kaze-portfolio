58c5afc29404cdb6b2db5a620ab37e2fd96687cb
HayateIwamoto <iwamoto.hayate.613@gmail.com>
2025-07-21 19:38:00 +0900

    feat: カテゴリーページを追加

---
+++ src/app/works/category/[id]/page/[current]/page.tsx
@@ -0,0 +1,78 @@
+import { notFound } from "next/navigation";
+import { WORKS_LIST_LIMIT } from "@/constants";
+import {
+  getWorksList,
+  getWorksCategoryList,
+  getCategoryDetail,
+} from "@/lib/api/microcms/crient";
+import { Breadcrumbs } from "@/components/ui/BreadCrumb";
+import WorksCard from "@/components/ui/Card/WorksCard";
+import CardsContainer from "@/components/ui/CardsContainer";
+import CategoryFilter from "@/components/ui/CategoryFilter";
+import Pagination from "@/components/ui/Pagination";
+import styles from "../../../../page.module.scss";
+type Props = {
+  params: {
+    id: string;
+    current: string;
+  };
+};
+
+export default async function WorkListPage({ params }: Props) {
+  const id = params.id;
+  const current = parseInt(params.current as string, 10);
+
+  if (Number.isNaN(current) || current < 1) {
+    notFound();
+  }
+
+  const { contents: works, totalCount } = await getWorksList({
+    limit: WORKS_LIST_LIMIT,
+    offset: WORKS_LIST_LIMIT * (current - 1),
+    filters: `category[equals]${id}`,
+  });
+  const { contents: categories } = await getWorksCategoryList();
+
+  const category = await getCategoryDetail(id).catch(notFound);
+
+  return (
+    <>
+      <Breadcrumbs
+        items={[
+          { name: "Works", href: "/works" },
+          { name: category.name }
+        ]}
+      />
+
+      <div className="l-container">
+        <div className={`${styles["p-works"]}`}>
+          <div className="inner">
+            <CategoryFilter
+              categories={categories}
+              basePath="works"
+              currentId={id}
+            />
+             {works.length === 0 ? (
+              <p className="nopost-message">現在投稿はございません。</p>
+            ) : (
+              <>
+               <CardsContainer>
+              {works.map((article) => (
+                <WorksCard works={article} key={article.id} />
+              ))}
+            </CardsContainer>
+            <Pagination
+              totalCount={totalCount}
+              perpage={WORKS_LIST_LIMIT}
+              basePath={`works/category/${id}`}
+              current={current}
+            />
+              </>
+            )}
+            
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
