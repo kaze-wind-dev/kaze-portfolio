8a7e16eacdc3b701c4170492f6e6c37b8fc3ec06
HayateIwamoto <iwamoto.hayate.613@gmail.com>
2025-07-23 00:40:23 +0900

    fix: WorksPageLayoutの修正

--- src/app/works/layout.tsx
+++ src/app/works/layout.tsx
@@ -1,31 +1,14 @@
 import type { Metadata } from "next";
-import PageLayout from "@/components/layout/PageLayout";
 
 export const metadata: Metadata = {
   title: "Works",
   description: "私が作成した作品をご紹介いたします。",
 };
 
-export default function WorksLayout({
+export default function WorksPageLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-      <PageLayout
-        hero={{
-          heading: "Works",
-          subTitle: "作品紹介",
-          pageDescription: (
-            <>
-              現在のスキルを使用して作成した作品たちです。
-              <br />
-              デザイン的な見た目よりも、コードの美しさや保守性を意識して作成いたしました。
-            </>
-          ),
-        }}
-      >
-        {children}
-      </PageLayout>
-  );
+  return <>{children}</>;
 }
