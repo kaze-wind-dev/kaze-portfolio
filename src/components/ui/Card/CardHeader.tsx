74c8bea32544caeaf4957422b199119c3d7143b4
HayateIwamoto <iwamoto.hayate.613@gmail.com>
2025-07-24 00:00:39 +0900

    feat: ArticleCardの追加

--- src/components/ui/Card/CardHeader.tsx
+++ src/components/ui/Card/CardHeader.tsx
@@ -4,21 +4,35 @@ import styles from "./index.module.scss";
 
 type CardHeaderProps = {
   title: string;
-  thumbnail?:  MicroCMSImage;
+  thumbnail?: MicroCMSImage;
+  emoji?: string;
   children?: React.ReactNode;
 };
 
-const CardHeader = ({ title, thumbnail, children }: CardHeaderProps) => {
+const CardHeader = ({ title, thumbnail, emoji, children }: CardHeaderProps) => {
   return (
     <header className={`${styles["c-card__header"]}`}>
-      <figure className={`${styles["c-card__thumbnail"]}`}>
-        <Image
-          src={thumbnail?.url || `/images/no_image.jpg`}
-          width={thumbnail?.height || 640}
-          height={thumbnail?.width || 480}
-          alt={thumbnail?.alt || `no image`}
-        />
-      </figure>
+      {thumbnail ? (
+        <figure className={`${styles["c-card__thumbnail"]}`}>
+          <Image
+            src={thumbnail?.url || `/images/no_image.jpg`}
+            width={thumbnail?.height || 640}
+            height={thumbnail?.width || 480}
+            alt={thumbnail?.alt || `no image`}
+          />
+        </figure>
+      ) : emoji ? (
+        <div className={`${styles["c-card__emoji"]}`}>{emoji}</div>
+      ) : (
+        <figure className={`${styles["c-card__thumbnail"]}`}>
+          <Image
+            src={`/images/no_image.jpg`}
+            width={640}
+            height={480}
+            alt={`no image`}
+          />
+        </figure>
+      )}
       <h3 className={`${styles["c-card__title"]}`}>{title}</h3>
       {children}
     </header>
