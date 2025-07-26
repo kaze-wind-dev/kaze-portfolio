f0d58ee52d4be52d97798250986c90bf67edce95
HayateIwamoto <iwamoto.hayate.613@gmail.com>
2025-07-26 23:23:13 +0900

    delete: ソート機能を削除し、utilsへ移動

--- src/lib/api/zennFunctions.ts
+++ src/lib/api/zennFunctions.ts
@@ -1,5 +1,4 @@
 import zennAPIClient from "./zennClient";
-import type { ZennArticle } from "@/types/zenn";
 
 if (!process.env.ZENN_USERNAME) {
   throw new Error("usernameが設定されていません");
@@ -9,24 +8,4 @@ const client = zennAPIClient(process.env.ZENN_USERNAME);
 
 export async function getZennArticles() {
   return await client.get();
-}
-
-type getZennSortArticlesProps = {
-  sortKey: keyof ZennArticle;
-  order?: "asc" | "desc";
-};
-
-export async function getZennSortArticles({
-  sortKey,
-  order = "desc",
-}: getZennSortArticlesProps) {
-  const fetchData = await client.get();
-  const articles = fetchData.articles;
-  if (order == "asc") {
-    return [...articles].sort((a, b) => a[sortKey] - b[sortKey]);
-  } else if (order == "desc") {
-    return [...articles].sort((a, b) => b[sortKey] - a[sortKey]);
-  } else {
-    return [...articles];
-  }
 }
