3b3ef2e4f766f0fb9cb386c1172f9eafd70bd13b
HayateIwamoto <iwamoto.hayate.613@gmail.com>
2025-08-03 23:25:14 +0900

    fix: topSkillの追加

---
+++ src/components/sections/TopSkill/index.tsx
@@ -0,0 +1,174 @@
+"use client";
+import Image from "next/image";
+import { useState, useRef, useEffect } from "react";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import { AutoScroll } from "@splidejs/splide-extension-auto-scroll";
+
+import Button from "@/components/ui/Button";
+import SectionTitle from "@/components/ui/SectionTitle";
+import { useInview } from "@/hooks/useInview";
+import "@splidejs/react-splide/css";
+import styles from "./index.module.scss";
+
+const skillLogos = [
+  {
+    id: "html",
+    src: "/images/html_logo.svg",
+    alt: "HTML5",
+    width: 100,
+    height: 100,
+  },
+  {
+    id: "css",
+    src: "/images/css_logo.svg",
+    alt: "CSS",
+    width: 100,
+    height: 100,
+  },
+  {
+    id: "sass",
+    src: "/images/sass_logo.svg",
+    alt: "SASS",
+    width: 133,
+    height: 100,
+  },
+  {
+    id: "javascript",
+    src: "/images/js_logo.svg",
+    alt: "JavaScript",
+    width: 100,
+    height: 100,
+  },
+  {
+    id: "typescript",
+    src: "/images/ts_logo.svg",
+    alt: "TypeScript",
+    width: 100,
+    height: 100,
+  },
+  {
+    id: "next",
+    src: "/images/nextjs_logo.svg",
+    alt: "Next.js",
+    width: 363,
+    height: 74,
+  },
+  {
+    id: "react",
+    src: "/images/react_logo.svg",
+    alt: "react",
+    width: 123,
+    height: 109,
+  },
+  {
+    id: "php",
+    src: "/images/php_logo.svg",
+    alt: "PHP",
+    width: 141,
+    height: 71,
+  },
+  {
+    id: "microcms",
+    src: "/images/microcms_logo.svg",
+    alt: "microCMS",
+    width: 447,
+    height: 100,
+  },
+  {
+    id: "vercel",
+    src: "/images/vercel_logo.svg",
+    alt: "Vercel",
+    width: 409,
+    height: 81,
+  },
+  {
+    id: "github",
+    src: "/images/github_logo.svg",
+    alt: "GitHub",
+    width: 100,
+    height: 98,
+  },
+];
+
+const TopSkill = () => {
+  const ref = useRef<HTMLElement>(null);
+  const splideRef = useRef(null);
+
+  const splideOptions = {
+    type: "loop",
+    focus: "center",
+    gap: "1.5rem",
+    arrows: false,
+    pagination: false,
+    autoWidth: true,
+    clones: 10,
+    cloneStatus: true,
+    autoScroll: {
+      speed: 0.75,
+      autoStart: true,
+      pauseOnHover: true,
+    },
+  };
+  const [inview, setInview] = useState(false);
+
+  useInview<HTMLElement>({ ref, setInview, once: false });
+  useEffect(() => {
+    if (splideRef.current && splideRef.current.splide) {
+      if (inview) {
+        splideRef.current.splide.Components?.AutoScroll.play();
+      } else {
+        splideRef.current.splide.Components?.AutoScroll.pause();
+      }
+    }
+  }, [inview]);
+  
+  return (
+    <section className={styles["p-topSkill"]} ref={ref}>
+      <SectionTitle
+        className={styles["p-topSkill__title"]}
+        heading={<>Skill</>}
+        text={<>スキル概要</>}
+      />
+      <div className={styles["p-topSkill__text-area"]}>
+        <p className={styles["p-topSkill__text"]}>
+          HTML・Sass・PHP・自社CMSをメインにサイト制作を行っています。
+          <br />
+          現在の会社へと就職後、およそ3年半で200以上ものサイト構築に携わりました。
+        </p>
+        <p className={styles["p-topSkill__text"]}>
+          現在はフロントエンドエンジニアを目指して、React.jsやNext.jsなどモダン開発を学んでいます。
+        </p>
+      </div>
+      <Splide
+        options={splideOptions}
+        aria-label="実務や学習で使用している言語"
+        tag="section"
+        extensions={{ AutoScroll }}
+        className={styles["p-topSkill__slider"]}
+        ref={splideRef}
+      >
+        {skillLogos.map((logo) => {
+          const uniqueClassName = `p-topSkill__slider-slide--${logo.id}`;
+          return (
+            <SplideSlide
+              className={`${styles["p-topSkill__slider-slide"]} ${styles[uniqueClassName]}`}
+              key={logo.id}
+            >
+              <Image
+                src={logo.src}
+                alt={logo.alt}
+                width={logo.width}
+                height={logo.height}
+              />
+            </SplideSlide>
+          );
+        })}
+      </Splide>
+      <Button className={styles["p-topSkill__button"]} href="/about/#skills">
+        スキルについて
+      </Button>
+    </section>
+  );
+};
+
+export default TopSkill;
