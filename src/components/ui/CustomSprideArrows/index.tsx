830e5716e033d1a880e406883b25dd6f19dbc67d
HayateIwamoto <iwamoto.hayate.613@gmail.com>
2025-08-05 00:05:16 +0900

    feat: CustomSplideArrowsの追加

---
+++ src/components/ui/CustomSprideArrows/index.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { HiOutlineArrowNarrowRight,HiOutlineArrowNarrowLeft } from "react-icons/hi";
+
+import styles from "./index.module.scss";
+
+type Props = {
+  splideRef: React.RefObject<{ go: (direction: string) => void } | null>;
+};
+
+const CustomSplideArrows = ({ splideRef }: Props) => {
+  const handlePrev = () => {
+    splideRef.current?.go("-1");
+  };
+  const handleNext = () => {
+    splideRef.current?.go("+1");
+  };
+  return (
+    <div className={styles["c-customSplide-arrows"]}>
+      <button
+        className={`${styles["c-customSplide-arrow"]} ${styles["c-customSplide-arrow--prev"]}`}
+        onClick={handlePrev}
+        aria-label="前のスライドへ"
+      >
+        <HiOutlineArrowNarrowLeft className={styles['c-customSplide-arrow__icon']}/>
+      </button>
+      <button
+        className={`${styles["c-customSplide-arrow"]} ${styles["c-customSplide-arrow--newst"]}`}
+        onClick={handleNext}
+        aria-label="次のスライドへ"
+        >
+        <HiOutlineArrowNarrowRight className={styles['c-customSplide-arrow__icon']}/>
+      </button>
+    </div>
+  );
+};
+
+export default CustomSplideArrows;
