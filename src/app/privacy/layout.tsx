7c378c78dfe2a9aa2bb37a19bbc653afd926e152
HayateIwamoto <iwamoto.hayate.613@gmail.com>
2025-08-09 00:09:29 +0900

    feat: Privacyページのlayout.tsxの追加

---
+++ src/app/privacy/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import PageLayout from "@/components/layout/PageLayout";
+
+export const metadata: Metadata = {
+  title: "Privacy Policy",
+  description: "Kaze Portfolioサイトのプライバシーポリシーページです",
+};
+
+export default function AboutLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <PageLayout
+      hero={{
+        heading: "Privacy Policy",
+        subTitle: "プライバシーポリシー",
+        pageDescription:"当サイトでは、お客様の個人情報を以下の通り取り扱います。"
+        
+      }}
+    >
+      {children}
+    </PageLayout>
+  );
+}
