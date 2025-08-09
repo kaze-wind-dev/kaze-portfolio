464590bf211dc90651961b334e5a8b11eacacedf
iwamoto <iwa.1938wr.web@gmail.com>
2025-08-10 00:06:43 +0900

    feat: sitemapページのlayout.tsxの追加

---
+++ src/app/site/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import PageLayout from "@/components/layout/PageLayout";
+
+export const metadata: Metadata = {
+  title: "Site Map",
+  description: "サイトマップコンテンツのページです",
+};
+
+export default function SteMapLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <PageLayout
+      hero={{
+        heading: "Site Map",
+        subTitle: "サイトマップコンテンツ",
+      }}
+    >
+      {children}
+    </PageLayout>
+  );
+}
