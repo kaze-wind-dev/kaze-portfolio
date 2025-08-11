690bc6da591945d2559b3ae5575c5bf9fe870657
iwamoto <iwa.1938wr.web@gmail.com>
2025-08-11 22:52:50 +0900

    content: Aboutページのコンテンツを編集

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -16,12 +16,12 @@ const skillsSectionItems: skillContentList = [
   {
     title: "実務経験（4年）",
     technologies: ["HTML", "CSS", "Sass", "PHP", "jQuery", "EC-CUBE"],
-    text: "HTML/CSS(SCSS)を使用したコーディング業務で200件以上のサイト構築。\n自社CMSとVanillaPHPでの開発。\n入社2年目からPagemanager（ページ管理ツール）を設計・開発し、 関数とクラス構文を活用したコードを実装。今年の7月に社内にリリースしました。",
+    text: "HTML/CSS(SCSS)を使用したコーディング業務で200件以上のサイト構築しました。\n自社CMSとVanillaPHPでのWebサイトを開発しております。\n入社2年目から、slugやページ名のミスが多かったことをきっかけに、これらを一元管理できないかと考え、Pagemanager（ページ情報管理ツール）を設計・開発しました。配列を使用した情報管理と、関数とクラス構文を活用したコードを実装しています。今年の7月に社内にリリースを行い、現在は運用中です。\n加えて、簡単なマネジメント業務も行っており、支店内のコーダーの構築能力や案件の納期を踏まえて案件の割り振りを行なっております。\nまた、最近では業務で対応可能な範囲でサイト表示改善を行っており、直近の未公開のサイトでは一部のページでPageSpeedInsightsのスコア100を達成しました。",
   },
   {
     title: "個人学習",
     technologies: ["React", "Next.js", "TypeScript", "JavaScript", "GitHub"],
-    text: "ポートフォリオの掲載の有無を合わせて、6プロジェクトを作成。\nそのうちポートフォリオ掲載は3作品。\nフロントエンドエンジニアへの転職を目指して、今年の4月よりReact/Next.jsの学習を本格的に開始。\n過去にもVanillaJSやReactを断続的に学習していましたが、 現在は継続的に取り組み、 現在はNext.js + microCMSでのJamstack開発が可能なレベルまで到達。",
+    text: "ポートフォリオの掲載の有無を合わせて、過去に6プロジェクトを作成しました。\nそのうちポートフォリオ掲載は3作品を掲載しています。\nフロントエンドエンジニアへの転職を目指して、今年の4月よりReact/Next.jsの学習と作品制作を開始。\n過去にもVanillaJSやReactを断続的に学習していましたが、現在は継続的に取り組んでいます。\nNext.js + microCMSでのJamstack構成での開発が可能なレベルまで到達しました。\n「世界一流のエンジニアの思考法」という本を読んでからは特に基本に忠実になろうと意識して学習に取り組んでいます。\n今後は簡易的なCMSやログイン機能付のTODOアプリを作成してみたいと考えております。",
   },
 ];
 
@@ -35,26 +35,28 @@ export default async function AboutPage() {
             <div className={`${styles["p-profile__container"]}`}>
               <div className={`${styles["p-profile__left"]}`}>
                 <hgroup className={`${styles["p-profile__name"]}`}>
-                  <h2 className={`${styles["p-profile__name-en"]}`}>Kaze</h2>
-                  <span className={`${styles["p-profile__name-ja"]}`}>かぜ</span>
+                  <h2 className={`${styles["p-profile__name-en"]}`}>Iwamoto Hayate</h2>
+                  <span className={`${styles["p-profile__name-ja"]}`}>岩本 颯</span>
                 </hgroup>
                 <p className={`${styles["p-profile__position"]}`}>コーダー</p>
                 <p className={`${styles["p-profile__text"]}`}>
                   はじめまして。
                   <br />
-                  東京都内でコーダーをしている、Kaze（かぜ）といいます。
+                  東京都内でコーダーとして従事している、岩本颯（いわもとはやて）といいます。<br/>
+                  3年間、コーダとして従事しておりますが、常に『限られた期間のなかでいかに早く、綺麗に構築をするか』を考えてコーディングを行い、継続的に構築方法をアップデートするようして参りました。<br/>
+                  ヘッドレスCMSを使用したサイト構築に特に興味があり、Jamstack構成のサイト制作やアプリ開発にも興味を持っています。<br/>
+                  現在はフロントエンドエンジニアを目指して、学習と転職活動を行なっております。<br/>
+                  将来的にはバックエンドへ分野を広げ、フロントを主戦場としたフルスタックエンジニアになることを目標としています。<br/>
                 </p>
               </div>
               <div className={`${styles["p-profile__right"]}`}>
                 <div className={`${styles["p-profile__box"]}`}>
                   <div  className={`${styles["p-profile__image"]}`}>
-
                   <Image
                     src="/images/no_image.jpg"
                     alt="noimage"
                     width={160}
                     height={160}
-                   
                   />
                   </div>
                   <IconList />
@@ -121,7 +123,9 @@ export default async function AboutPage() {
                   <p
                     className={`${styles["p-experience__list-item__content"]}`}
                   >
-                    高校卒業後、市役所で公務員として2年間従事。
+                    高校卒業後、市役所で公務員（土木系技術職）として2年間従事。<br/>
+                    下水道工事の工事発注や現場管理、窓口対応などを担当する。<br/>
+                    上司だった方から効率化や仕事に対する姿勢を学ぶが、仕事が合わずに悩み、Web業界への転身を決意して退職。
                   </p>
                 </li>
                 <li className={`${styles["p-experience__list-item"]}`}>
@@ -131,7 +135,7 @@ export default async function AboutPage() {
                   <p
                     className={`${styles["p-experience__list-item__content"]}`}
                   >
-                    Web業界への転身を決意し、専門学校へ入学。
+                    情報学園専門学校へ入学。<br/>しかし授業はほとんど受けず、Udemyや書籍、ネットの情報からデザインの基礎+HTML・CSS（SCSS）のコーディングとvanillaJSの記述を独学。
                   </p>
                 </li>
                 <li className={`${styles["p-experience__list-item"]}`}>
@@ -143,7 +147,19 @@ export default async function AboutPage() {
                   >
                     現在の会社へ就職。
                     <br />
-                    コーダーとして従事する。
+                    コーダーとして従事し、現在に至るまでに200以上のサイトを構築。
+                    通常のサイト構築の業務以外にも、採用担当・新卒教育・改善業務・案件管理を担当する。
+                  </p>
+                </li>
+                <li className={`${styles["p-experience__list-item"]}`}>
+                  <p className={`${styles["p-experience__list-item__year"]}`}>
+                    2025年4月
+                  </p>
+                  <p
+                    className={`${styles["p-experience__list-item__content"]}`}
+                  >
+                  現在の会社でのスキルアップに限界点を感じ、フロントエンドエンジニアとして転職することを本気で決意。
+                  本格的にReact.jsやNext.jsを学びながら、ポートフォリオ作成・転職活動中。
                   </p>
                 </li>
               </ul>
