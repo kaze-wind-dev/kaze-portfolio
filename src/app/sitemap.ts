ed3ad89a42723eb17e0ab355f33d4d9cc3709b4b
HayateIwamoto <iwamoto.hayate.613@gmail.com>
2025-08-15 22:43:16 +0900

    feat: sitemap.xmlの作成

---
+++ src/app/sitemap.ts
@@ -0,0 +1,57 @@
+import { MetadataRoute } from "next";
+import { getAllWorksCategoryList, getAllWorksList } from "@/lib/api/microcms";
+
+const buildUrl = (path?: string) =>
+  `https://portfolio.kaze-develop.com${path ?? ""}`;
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const WorksList = await getAllWorksList();
+  const WorksCategoryList = await getAllWorksCategoryList();
+  const now = new Date();
+  const worksUrls: MetadataRoute.Sitemap = WorksList.map((content) => {
+    return {
+      url: buildUrl(`/works/${content.id}`),
+      lastModified: now,
+    };
+  });
+  const worksCategoryUrls: MetadataRoute.Sitemap = WorksCategoryList.map(
+    (content) => {
+      return {
+        url: buildUrl(`/works/category/${content.id}`),
+        lastModified: now,
+      };
+    }
+  );
+  return [
+    {
+      url: buildUrl(),
+      lastModified: now,
+    },
+    {
+      url: buildUrl("/works"),
+      lastModified: now,
+    },
+    ...worksCategoryUrls,
+    ...worksUrls,
+    {
+      url: buildUrl("/articles"),
+      lastModified: now,
+    },
+    {
+      url: buildUrl("/about"),
+      lastModified: now,
+    },
+    {
+      url: buildUrl("/contact"),
+      lastModified: now,
+    },
+    {
+      url: buildUrl("/site"),
+      lastModified: now,
+    },
+    {
+      url: buildUrl("/privacy"),
+      lastModified: now,
+    },
+  ];
+}
