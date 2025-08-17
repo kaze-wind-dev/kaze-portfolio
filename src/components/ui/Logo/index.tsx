20a98e4c42f2cabd77505d738a200c4d87d5390d
HayateIwamoto <iwamoto.hayate.613@gmail.com>
2025-08-17 15:09:50 +0900

    refactor: LogoImageをLogoMarkImageに変更し、テキストを追加

--- src/components/ui/Logo/index.tsx
+++ src/components/ui/Logo/index.tsx
@@ -1,22 +1,27 @@
-
-import LogoImage from "../LogoImage";
+import LogoMarkImage from "../LogoImage/LogoMarkImage";
 import Link from "next/link";
 type Props = {
   width?: number;
   height?: number;
   link?: boolean;
   className?: string;
-}
+};
 
-const Logo = ({ width = 581, height = 104, link = false, className }: Props) => {
-  return (
-    link ? 
-      <Link href="/" className={`${className || ""}`}>
-        <LogoImage width={width} height={height} />
-      </Link>
-    :
-    <div className={className}>
-      <LogoImage width={width} height={height} />
+const Logo = ({
+  width = 104,
+  height = 104,
+  link = false,
+  className,
+}: Props) => {
+  return link ? (
+    <Link href="/" className={`logo ${className || ""}`}>
+      <LogoMarkImage width={width} height={height} />
+      <span className="logo-text">Kaze Portfolio</span>
+    </Link>
+  ) : (
+    <div className={`logo ${className || ""}`}>
+      <LogoMarkImage width={width} height={height} />
+      <span className="logo-text">Kaze Portfolio</span>
     </div>
   );
 };
