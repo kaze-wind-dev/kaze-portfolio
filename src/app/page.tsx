15f833e8cf23e9d6d790fe10e1468122596d08a2
HayateIwamoto <iwamoto.hayate.613@gmail.com>
2025-08-31 23:28:40 +0900

    feat: TypingAnimationとscroll-guideの追加　refactor: コンポーネント分割

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-import Image from "next/image";
 import Logo from "@/components/ui/Logo";
 import Button from "@/components/ui/Button";
-import SectionTitle from "@/components/ui/SectionTitle";
 import TopSkill from "@/components/sections/TopSkill";
 import TopWorks from "@/components/sections/topWorks";
 import TopArticles from "@/components/sections/TopArticles";
+import TopAbout from "@/components/sections/TopAbout";
+import TopCta from "@/components/sections/TopCta";
+import TypingAnimation from "@/components/TypingAnimation";
 import { getWorksList } from "@/lib/api/microcms";
 import { getZennArticles } from "@/lib/api/zennFunctions";
 import styles from "./page.module.scss";
@@ -22,7 +23,14 @@ export default async function Home() {
       <section className={`${styles["p-topHero"]} `}>
         <div className={`${styles["p-topHero__inner"]} inner`}>
           <h1 className={`${styles["p-topHero__heading"]}`}>
-            コーダーからフロントエンドエンジニアへ
+            
+            <TypingAnimation
+              texts={["コーダーからフロントエンドエンジニアへ"]}
+              typingSpeed={0.1}
+              pauseTime={3}
+              loop = {false}
+            />
+            
             <br />
             3年間培った経験を活かし、次のステージを目指しています。
           </h1>
@@ -32,69 +40,19 @@ export default async function Home() {
               作品を見る
             </Button>
           </div>
+           <div className={`${styles["p-topHero__scroll-guide"]}`}>
+            <span className={`${styles["p-topHero__scroll-guide__text"]}`}>
+              scroll
+            </span>
+            <div className={`${styles["p-topHero__scroll-guide__bar"]}`}></div>
+          </div>
         </div>
       </section>
       <TopWorks works={works} />
       <TopArticles articles={zennArticlesData.articles} />
       <TopSkill />
-      <section className={styles["p-topAbout"]}>
-        <div className={`${styles["p-topAbout__inner"]} inner`}>
-          <SectionTitle
-            className={styles["p-topAbout__title"]}
-            heading={<>About</>}
-            text={<>私について</>}
-            position="left"
-          />
-          <div className={styles["p-topAbout__text-area"]}>
-            <p className={styles["p-topAbout__text"]}>
-              東京都内でコーダーとして3年間従事。
-              <br />
-              200件以上のWebサイト構築実績があります。
-              <br />
-              現在はReact/Next.jsを学習し、フロントエンドエンジニアへの転職を目指しています。
-            </p>
-            <p className={styles["p-topAbout__text"]}>
-              フロントエンドエンジニアとして、より価値のあるWeb開発に挑戦したいと考えています。
-            </p>
-          </div>
-          <Button
-            className={styles["p-topAbout__button"]}
-            href="/about"
-            aria-label="制作者について詳しく見る"
-          >
-            詳しく見る
-          </Button>
-          <div className={styles["p-topAbout__image"]}>
-            <Image
-              src="/images/avatar.jpg"
-              width={360}
-              height={360}
-              alt="アバター画像"
-              loading="lazy"
-            ></Image>
-          </div>
-        </div>
-      </section>
-      <section className={styles["p-topCta"]}>
-        <div className={`${styles["p-topCta__inner"]} inner`}>
-          <SectionTitle
-            className={styles["p-topCta__title"]}
-            heading={<>Contact</>}
-            text={<>お問い合わせ</>}
-          />
-          <p className={styles["p-topCta__text"]}>
-            お気づきの点や気になることがございましたら
-            <br />
-            お気軽にお問い合わせください。
-          </p>
-          <Button
-            className={`${styles["p-topCta__button"]} mx-center`}
-            href="/contact"
-          >
-            お問い合わせはこちら
-          </Button>
-        </div>
-      </section>
+      <TopAbout />
+      <TopCta />
     </>
   );
 }
